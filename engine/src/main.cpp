c171836911d5706e2aa6c04c3e05edcdee330d32
Alex Allman <alexallmandev+contact@gmail.com>
2022-06-21 15:49:49 +0100

    Quad drawn using EBO and little test tasks above to play around with

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -11,6 +11,20 @@
 #include <tuple>
 
 
+/*
+Exercises
+
+To really get a good grasp of the concepts discussed a few exercises were set up. It is advised to work through them before continuing to the next subject to make sure you get a good grasp of what's going on.
+    Try to draw 2 triangles next to each other using glDrawArrays by adding more vertices to your data: solution.
+    Now create the same 2 triangles using two different VAOs and VBOs for their data: solution.
+    Create two shader programs where the second program uses a different fragment shader that outputs the color yellow; draw both triangles again where one outputs the color yellow: solution.
+*/
+
+
+bool rect_active = false;
+bool key_pressed = false;
+GLuint fill_type = GL_FILL;
+
 void error_callback(int err, const char* desc) {
     std::cout << "An glfw error has occured: " << desc << std::endl;
 }
@@ -21,15 +35,35 @@ void framebuffer_size_callback(GLFWwindow* wnd, int new_width, int new_height) {
 }
 
 static void key_callback(GLFWwindow* wnd, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(wnd, GLFW_TRUE);
+    if (!key_pressed){
+        if (key == GLFW_KEY_ESCAPE && !key_pressed) {
+            glfwSetWindowShouldClose(wnd, GLFW_TRUE);
+        }
+        else if (key == GLFW_KEY_W) { 
+            fill_type = fill_type == GL_FILL ? GL_LINE : GL_FILL;
+            glPolygonMode(GL_FRONT_AND_BACK, fill_type);
+        }
+        else if (key == GLFW_KEY_SPACE) { 
+            rect_active = !rect_active;
+        }
+    }
+
+    if (action == GLFW_PRESS) {
+        key_pressed = true;
+    }
+    else if (action == GLFW_RELEASE) {
+        key_pressed = false;
     }
 }
 
 void processInput(GLFWwindow* wnd) { 
-    if (glfwGetKey(wnd, GLFW_KEY_SPACE) == GLFW_PRESS) { 
-        //Do some rendering shite or something idk
-    }
+    // if (glfwGetKey(wnd, GLFW_KEY_SPACE) == GLFW_PRESS && !key_pressed) {
+    //     rect_active = !rect_active;
+    // }
+    // else if (glfwGetKey(wnd, GLFW_KEY_W) == GLFW_PRESS && !key_pressed) {
+    //     fill_type = fill_type == GL_FILL ? GL_LINE : GL_FILL;
+    //     glPolygonMode(GL_FRONT_AND_BACK, fill_type);
+    // }
 }
 
 // These are normalized device coords (NDC) but typically screen coords are 0,0 top left
@@ -39,14 +73,27 @@ float tri_verts[] = {
     0.0f,  0.5f, 0.0f,
 };
 
-const char *vertexShaderSource = "#version 330 core\n"
+//Drawing a rect using the element buffer object (basically an index buffer)
+float rect_verts[] = {
+    0.5f, 0.5f, 0.0f,       //TR
+    0.5f, -0.5f, 0.0f,      //BR
+    -0.5f, -0.5f, 0.0f,     //BL
+    -0.5f, 0.5f, 0.0f       //TL
+};
+
+unsigned int rect_inds[] = {
+    0, 1, 3,
+    1, 2, 3
+};
+
+const char* vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColour;\n"
     "void main()\n"
     "{\n"
@@ -96,7 +143,7 @@ int main(int, char**) {
     int vsync = 1; //1 is on 0 is off
     glfwSwapInterval(vsync);
 
-    ////Vertex buffer + Array
+    ////Vertex buffer + Array Single tri
     //Set up a Vertex Array Object to store this configuration info into
     GLuint vao_id;
     glGenVertexArrays(1, &vao_id);
@@ -122,6 +169,28 @@ int main(int, char**) {
         static_cast<void*>(0)   //Offset to where we should begin in the buffer
     );
     glEnableVertexAttribArray(0);   //Since our previously made VBO is still bound this vertex attrib pointer refers to the data stored within that VBO
+
+    ////Vertex buffer + VAO + EBO for rect
+    GLuint rect_vao_io;
+    glGenVertexArrays(1, &rect_vao_io);
+
+    GLuint rect_vbo_id;
+    glGenBuffers(1, &rect_vbo_id);
+
+    GLuint rect_ebo_id;
+    glGenBuffers(1, &rect_ebo_id);
+
+    //bind the VAO for the upcoming configuration
+    glBindVertexArray(rect_vao_io);
+
+    glBindBuffer(GL_ARRAY_BUFFER, rect_vbo_id);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rect_verts), rect_verts, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
+    glEnableVertexAttribArray(0);   //This binds this specific Vertex Attrib Array to the layout in the vertex shader, redudant here due to the previous one but done for clarity sake
+
+    //Bind the EBO config to the VAO; VAO pays attention to both binds and unbinds so ensure the VAO is unbound before unbinding EBO or VBO otherwise it wont be bound on the VAO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rect_ebo_id);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rect_inds), rect_inds, GL_STATIC_DRAW);
 
     ////Compilation of vertex shader
     GLuint vertexShader;
@@ -183,7 +252,7 @@ int main(int, char**) {
         //// Input Processing
         double time_since_creation = glfwGetTime();
         //Alternative to processing Input, instead of using keypress callback, use a glfw specific func. I guess this specifically gets inputs at the start of a frame instead of processing them at the end
-        processInput(window);
+        // processInput(window);
 
         //// Rendering commands
         //Set a clear colour before using GL clear to flush the existing colour buffer with the new col
@@ -192,9 +261,15 @@ int main(int, char**) {
 
         //DRAW THE TRIANGLE. Always ensure you set the state desired for the rendering as to not inhert a previous state
         glUseProgram(shaderProgram);
-        glBindVertexArray(vao_id);
-        glDrawArrays(GL_TRIANGLES, 0, 3);   //Args: GL_TRIANGES defines we wanna draw tris with the vert data, 0 for start index, 3 for the total length
 
+        if (rect_active){
+            glBindVertexArray(rect_vao_io);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL); //Indices ptr has 2 uses, a ptr to indicies if an ebo isnt bound, or an offset if an ebo is; null here as we want to being at 0
+        }
+        else{
+            glBindVertexArray(vao_id);
+            glDrawArrays(GL_TRIANGLES, 0, 3);   //Args: GL_TRIANGES defines we wanna draw tris with the vert data, 0 for start index, 3 for the total length
+        }
 
         //// Check events and swap buffers with new renders
         glfwSwapBuffers(window);
